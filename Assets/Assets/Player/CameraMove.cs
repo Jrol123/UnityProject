11f2572201fcd035e5443aec4454660ed50ab638
Artemii Popovkin <angap4@gmail.com>
2025-10-11 21:21:23 +1000

    fix lerp
    Я зачем-то делал через общую t-шку.
    
    Теперь переделал на нормальную скорость

--- Assets/Assets/Player/CameraMove.cs
+++ Assets/Assets/Player/CameraMove.cs
@@ -5,7 +5,6 @@ public class CameraMove : MonoBehaviour
     private Vector3 camOffset;
     [SerializeField] private Transform Player;
     [SerializeField] private float speed = 0.5f;
-    private float t = 0;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -26,8 +25,11 @@ public class CameraMove : MonoBehaviour
 
     void Moving()
     {
-        t += Time.deltaTime * speed;
-        Vector3 new_pos = new Vector3(Player.position.x, 0, Player.position.z) + camOffset;
-        transform.position = Vector3.Lerp(transform.position, new_pos, t * speed);
+        Vector3 targetPosition = new Vector3(Player.position.x, 0, Player.position.z) + camOffset;
+        transform.position = Vector3.Lerp(
+            transform.position,
+            targetPosition,
+            speed * Time.deltaTime
+        );
     }
 }
